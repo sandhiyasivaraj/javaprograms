2f646731bcbc5912a2e6f8833e705b5123071736
sandhiyasivaraj <34916422+sandhiyasivaraj@users.noreply.github.com>
2018-03-26 10:11:37 +0530

    Create 212.product_of_sum.java

---
+++ player/set22/212.product_of_sum.java
@@ -0,0 +1,40 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int N,i,j,sum1=0,sum2=0,product;
+		
+		Scanner in=new Scanner(System.in);
+		N=in.nextInt();
+		int a[][]=new int[N][N];
+		for(i=0;i<N;i++)
+		{
+			for(j=0;j<N;j++)
+			{
+				a[i][j]=in.nextInt();
+			}
+		}
+			for(i=0;i<N;i++)
+		{
+			sum1=sum1+a[i][i];
+		
+		}
+			for(i=N-1;i>=0;i--)
+		{
+			sum2=sum2+a[i][i];
+		
+		}
+			product=sum1*sum2;		
+			System.out.println(product);
+		
+		
+	}
+}
