1004fdf1a9a6a8707a3ef200814f0468dea2cd46
sandhiyasivaraj <34916422+sandhiyasivaraj@users.noreply.github.com>
2018-05-23 20:09:33 +0530

    Create 52.wordnumber.java

---
+++ begginer/set6/52.wordnumber.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Wordnumber{
+
+     public static void main(String []args){
+         int N,i,j;
+         Scanner in=new Scanner(System.in);
+         N=in.nextInt();
+         String word[]={"","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten"};
+         
+        System.out.print(word[N]);
+         
+     }
+}
