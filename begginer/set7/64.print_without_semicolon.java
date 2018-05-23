9ebfdc5e8f615ef5b0394ed43caa432a24702226
sandhiyasivaraj <34916422+sandhiyasivaraj@users.noreply.github.com>
2018-05-23 22:49:46 +0530

    Create 64.print_without_semicolon.java

---
+++ begginer/set7/64.print_without_semicolon.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Withoutsemicol{
+
+     public static void main(String []args){
+        String S;
+        Scanner in=new Scanner(System.in);
+        S=in.nextLine();
+       if(S!=null)
+       {
+           for(int i=1;i<2;System.out.println(S))
+           {
+               i++;
+           }
+       }
+     }
+}
