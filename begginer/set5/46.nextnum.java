7aee824b2e9531109c2e996ccdcbb9cd565d6460
sandhiyasivaraj <34916422+sandhiyasivaraj@users.noreply.github.com>
2018-05-23 21:57:26 +0530

    Create 46.nextnum.java

---
+++ begginer/set5/46.nextnum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Nextnum{
+
+     public static void main(String []args){
+         int N;
+         Scanner in=new Scanner(System.in);
+         N=in.nextInt();
+         System.out.print(N+1);
+     }
+}
