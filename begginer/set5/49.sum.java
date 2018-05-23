aa73cf8deb53af7479f1d6fdc36cd5d849390e1c
sandhiyasivaraj <34916422+sandhiyasivaraj@users.noreply.github.com>
2018-05-23 22:07:04 +0530

    Create 49.sum.java

---
+++ begginer/set5/49.sum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Sum{
+
+     public static void main(String []args){
+         int N1,N2;
+         Scanner in=new Scanner(System.in);
+         N1=in.nextInt();
+         N2=in.nextInt();
+         System.out.print(N1+N2);
+     }
+}
