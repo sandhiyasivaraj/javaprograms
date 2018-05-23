ffec1beb9660c2250a9fbc74fd3b08f67cd1f6dd
sandhiyasivaraj <34916422+sandhiyasivaraj@users.noreply.github.com>
2018-05-23 22:56:07 +0530

    Create 70.greater_power_of_two.java

---
+++ begginer/set7/70.greater_power_of_two.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class GreaterPower{
+
+     public static void main(String []args){
+        int N;
+        Scanner in=new Scanner(System.in);
+        N=in.nextInt();
+        System.out.println(N*2);
+     }
+}
