78d5330d79ed419a6940b39ce797e8a272f5db9d
sandhiyasivaraj <34916422+sandhiyasivaraj@users.noreply.github.com>
2018-06-24 15:35:17 +0530

    Create 74.greatest integer.java

---
+++ begginer/set8/74.greatest integer.java
@@ -0,0 +1,21 @@
+package beginner;
+
+importjava.util.Scanner;
+
+public class Greatest_integer {
+	privatestatic Scanner in;
+
+	public static intgreatestint(floatF) 
+	{
+		intf=(int)F;
+		returnf+1;
+	}
+
+	public static void main(String[] args) {
+		in = new Scanner(System.in);
+		floatF=in.nextFloat();
+		intf=greatestint(F);
+		System.out.print(f);
+	}
+
+}
