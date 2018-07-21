aa0a5a8f771343b0f7d58b1f7f561ab71b3db979
sandhiyasivaraj <34916422+sandhiyasivaraj@users.noreply.github.com>
2018-07-21 21:49:32 +0530

    Create 41.Power_Of_K.java

---
+++ player/set5/41.Power_Of_K.java
@@ -0,0 +1,20 @@
+
+package GuviPlayerSet5;
+
+import java.util.Scanner;
+
+public class PowerOfK {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int N=in.nextInt();
+        int K=in.nextInt();
+        while(N%K==0 && N!=0)
+        {
+            N=N/K;
+        }
+        if(N==1)
+            System.out.println("yes");
+        else
+            System.out.println("no");
+    }
+}
