72fef67171c0282a7f84cedd67a15c54d8783073
sandhiyasivaraj <34916422+sandhiyasivaraj@users.noreply.github.com>
2018-07-21 20:26:43 +0530

    Create 39.Power_Of_two.java

---
+++ player/set4/39.Power_Of_two.java
@@ -0,0 +1,19 @@
+
+package GuviPlayerSet4;
+
+import java.util.Scanner;
+
+public class PowerOfTwo {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int N=in.nextInt();
+        while(N%2==0 && N!=0)
+        {
+            N=N/2;
+        }
+        if(N==1)
+            System.out.println("yes");
+        else
+            System.out.println("no");
+    }
+}
