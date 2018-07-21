c2f55a6b7e12a70a8f7aabc8d6d1d46f9d825bff
sandhiyasivaraj <34916422+sandhiyasivaraj@users.noreply.github.com>
2018-07-21 21:18:35 +0530

    Create 40.WaysToFormAnumberUsing1and2.java

---
+++ player/set4/40.WaysToFormAnumberUsing1and2.java
@@ -0,0 +1,13 @@
+
+package GuviPlayerSet4;
+
+import java.util.Scanner;
+
+public class WaysToFormAnumberUsing1and2 {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int N=in.nextInt();
+        int ways=N/2+1;
+        System.out.println(ways);
+       }
+}
