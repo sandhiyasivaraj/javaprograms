ac9c94075bacc415540b53d99eccdd42e7372048
sandhiyasivaraj <34916422+sandhiyasivaraj@users.noreply.github.com>
2018-07-22 11:54:50 +0530

    Create 47.Interior_Angle_of_triangle.java

---
+++ player/set5/47.Interior_Angle_of_triangle.java
@@ -0,0 +1,21 @@
+package GuviPlayerSet5;
+import java.util.Scanner;
+public class Triangle {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int A1=in.nextInt();
+        int A2=in.nextInt();
+        int A3=in.nextInt();
+        if(A1!=0 && A2!=0 && A3!=0)
+        {
+            if(A1+A2+A3==180)
+            {
+                System.out.println("yes");
+            }
+            else
+                System.out.println("no");
+        }
+        else
+            System.out.println("no");
+    }
+}
